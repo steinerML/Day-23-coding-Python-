e7d3ebadecf4ac7ff1b3f6b813ef9e4d2627f74a
steinerML <102678863+steinerML@users.noreply.github.com>
2022-05-11 16:30:46 +0200

    Add files via upload

---
+++ Day23.py
@@ -0,0 +1,22 @@
+# Print 12 asterisks with the range() function
+for a in range(1,13):
+  print('*', end=" ")
+print()
+
+
+# Print 12 asterisk with a while() loop
+
+i = 1
+while i < 13:
+    print('*', end=" ")
+    i += 1
+print()
+
+#Another way of doing the same
+
+i = 1
+while i <= 12:
+    print('*', end=" ")
+    i += 1
+print()
+
